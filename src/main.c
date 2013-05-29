fddce7bd68057b1aab8d28a0d6366eaa6193724d
jeroenpeeters <jeroen@peetersweb.nl>
2013-05-29 10:31:31 -0700

    Text test

--- src/main.c
+++ src/main.c
@@ -5,18 +5,18 @@
 
 #define MY_UUID { 0xCF, 0x91, 0xE3, 0xEC, 0xB4, 0xB0, 0x43, 0x59, 0x8A, 0xD4, 0xAB, 0x10, 0x81, 0x8F, 0xE8, 0xF8 }
 PBL_APP_INFO(MY_UUID,
-             "Weather", "TMD",
+             "TestApp", "TMD",
              1, 0, /* App version */
              DEFAULT_MENU_ICON,
              APP_INFO_STANDARD_APP); //APP_INFO_WATCH_FACE
 
 Window window;
-
+TextLayer left_layer;
 
 void handle_init(AppContextRef ctx) {
   (void)ctx;
 
-  window_init(&window, "Window Name");
+  window_init(&window, "Main window");
   window_stack_push(&window, true /* Animated */);
 }
 
